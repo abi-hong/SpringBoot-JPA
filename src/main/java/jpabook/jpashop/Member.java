261d1418734cfd8fa26c68404c7446ce81021c1c
abi-hong <68041042+abi-hong@users.noreply.github.com>
2022-07-09 21:20:08 +0900

    feat: 프로젝트 설정, H2 DB 설정

---
+++ src/main/java/jpabook/jpashop/Member.java
@@ -0,0 +1,18 @@
+package jpabook.jpashop;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Getter @Setter
+public class Member {
+
+    // 식별자
+    @Id @GeneratedValue
+    private Long id;
+    private String username;
+}
