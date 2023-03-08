0219f26aa1e81524e33346c169342e25425a73c1
abi-hong <flffjakfwm20@gmail.com>
2023-03-08 23:06:24 +0900

    order, orderitem, item 비지니스 로직, 생성메서드

--- src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -22,4 +22,25 @@ public class OrderItem {
     private int orderPrice; //주문 당시 가격
     @Column
     private int count; //주문 당시 수량
+
+    //==생성 메서드==//
+    public static OrderItem createOrderItem(Item item, int orderPrice, int count) {
+        OrderItem orderItem = new OrderItem();
+        orderItem.setItem(item);
+        orderItem.setOrderPrice(orderPrice);
+        orderItem.setCount(count);
+
+        item.removeStock(count); //주문한 만큼 재고가 줄어야 함
+        return orderItem;
+    }
+
+    //==비지니스 로직==//
+    public void cancel() { //주문 취소되면, 수량을 취소된 만큼 높여줘야 함
+        getItem().addStock(count);
+    }
+
+    //==조회 로직==//
+    public int getTotalPrice() {
+        return getOrderPrice() * getCount();
+    }
 }
---
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -0,0 +1,11 @@
+package jpabook.jpashop.repository;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+@Repository
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class OrderRepository {
+}
