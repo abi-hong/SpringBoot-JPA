d65a2802543fcda10e2737e950e3524cf510ae52
abi-hong <flffjakfwm20@gmail.com>
2023-03-08 23:28:05 +0900

    orderService, 주문하기/주문취소

--- src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -1,11 +1,26 @@
 package jpabook.jpashop.repository;
 
+import jpabook.jpashop.domain.Order;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.EntityManager;
+import java.util.List;
+
 @Repository
-@Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class OrderRepository {
+
+    private final EntityManager em;
+
+    public void save(Order order) {
+        em.persist(order);
+    }
+
+    public Order findOne(Long id) {
+        return em.find(Order.class, id);
+    }
+
+    //public List<Order> findAll(OrderSearch orderSearch) {}
 }
